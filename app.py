30099dfb4894cf7fae43781fa3cb096d7824506c
Ashruth yenishetty <116997774+yashruth@users.noreply.github.com>
2026-04-13 12:57:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
 import streamlit as st
-import pandas as pd
-import joblib
-
-model = joblib.load("jewelry_model.pkl")
-
-st.title("AI Gold & Silver Jewelry Price Calculator")
-
-purity = st.number_input("Purity (18/22/24)")
+st.title("Gold & Silver Jewelry Price Calculator")
+purity = st.number_input("Purity (Example: 92.5 for silver, 22 for gold)")
 weight = st.number_input("Weight (grams)")
-rate = st.number_input("Metal Rate")
+rate = st.number_input("Metal Rate per gram")
 making = st.number_input("Making Charge %")
 wastage = st.number_input("Wastage %")
 stone_price = st.number_input("Stone Price")
-
 if st.button("Calculate Price"):
-
-    input_data = pd.DataFrame({
-        "purity":[purity],
-        "weight":[weight],
-        "rate":[rate],
-        "making":[making],
-        "wastage":[wastage],
-        "stone_price":[stone_price]
-    })
-
-    prediction = model.predict(input_data)
-
-    st.success(f"Predicted Price: ₹{prediction[0]:.2f}")
+    metal_value = weight * rate
+    making_charge = metal_value * making / 100
+    wastage_charge = metal_value * wastage / 100
+    subtotal = metal_value + making_charge + wastage_charge + stone_price
+    gst = subtotal * 0.03
+    final_price = subtotal + gst
+    st.success(f"Final Jewelry Price: ₹{final_price:.2f}")
+  
